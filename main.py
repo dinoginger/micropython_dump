14daec71e7a38b205570bb4994bf0407c2b816ad
marco <marcothemars@gmail.com>
2023-01-05 16:08:35 +0100

    Photosensor reading values (ranging from 0 to 100) + constants

---
+++ main.py
@@ -0,0 +1,38 @@
+from machine import ADC, Pin
+import time
+
+DEFAULT_ENTRY_PIN = 13
+DEFAULT_ADC_PIN = 12
+
+
+class Lab1_machine:
+
+    _info_pin = None
+    _entry_pin = None
+
+    def __init__(self, adc, entr): #
+        Pin(adc).value(1)
+        self._info_pin = ADC(Pin(adc))
+
+        self._info_pin.atten(ADC.ATTN_11DB) # volt [0.0 - 3.6v]
+
+        
+        self._entry_pin = Pin(entr, Pin.OUT)
+        self._entry_pin.value(1)
+
+    def reciever(self):
+        print((self._info_pin.read()/4095)*100) # values are from 0 to 4095 so we get percentages
+        time.sleep(0.1)
+        #test version
+        
+
+    def something(self):
+        pass
+
+
+Pin(DEFAULT_ENTRY_PIN, Pin.OUT).value(0)
+print("script is running")
+lab_machine = Lab1_machine(DEFAULT_ADC_PIN, DEFAULT_ENTRY_PIN)
+input("Press anything to launch\n")
+while True:
+    lab_machine.reciever()
