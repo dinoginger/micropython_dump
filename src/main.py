1b770ad00d89e7cca9b3d941e3f2f7e69ad79f4e
marco <marcothemars@gmail.com>
2023-01-09 22:58:50 +0100

    fixed photosensor

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ from machine import ADC, Pin, PWM
 import time
 
 # This is constants.
-DEFAULT_ENTRY_PIN = 13
-DEFAULT_ADC_PIN = 36
+DEFAULT_ENTRY_PIN = 32
+DEFAULT_ADC_PIN = 12
 FILE_PATH  = "/test.txt"
 
 class Lab1_controller:
@@ -20,11 +20,12 @@ class Lab1_controller:
 
         
         self._entry_pin = Pin(entr)
+
         self._info_pin = ADC(Pin(adc))
-        self._info_pin.atten(ADC.ATTN_11DB) # volt [0.0 - 3.6v]
+        self._info_pin.atten(self._info_pin.ATTN_11DB) # volt [0.0 - 3.6v]
 
         # 
-        self._pwm = PWM(Pin(DEFAULT_ENTRY_PIN), 5000)
+        self._pwm = PWM(Pin(DEFAULT_ENTRY_PIN), 40_000_000)
 
         self._entry_pin.value(1)  # Activating pins
  
@@ -33,7 +34,7 @@ class Lab1_controller:
     def reciever(self):
         info = self._info_pin.read()
         print(info) # values are from 0 to 4095 so we get percentages
-        time.sleep(0.01)
+        time.sleep(0.05)
         return info
         # TODO: finish it
 
@@ -54,11 +55,14 @@ lab_machine = Lab1_controller(DEFAULT_ADC_PIN, DEFAULT_ENTRY_PIN)
 input("Press anything to launch\n")
 
 file = open(FILE_PATH, "w")
-
+lll = []
 # bc thats the range for duty value.
 for i in range(0,1024):
+    print(i)
     lab_machine.change_light(i)
     info = lab_machine.reciever()
+    lll.append(info)
+
     print("writing {info}")
     file.write(str(info) + "\n")
 file.close()
